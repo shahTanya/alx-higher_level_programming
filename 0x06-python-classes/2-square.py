f23cef82364beade5f7ad182fc4d1ab136f7d09a
shahTanya <shahtania19@gmail.com>
2023-03-13 04:20:27 -0700

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Module for the square class'''
+
+
+class Square:
+    '''Creates a square class'''
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
